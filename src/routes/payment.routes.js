a42f017dcf0b7c11b92a3c46b7ee54c90fbe8c94
IMON <shuvradip.sarma2021@vitbhopal.ac.in>
2026-01-05 01:59:29 +0530

    payment logic done

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { generatePaymentOrder, verifyPaymentController } from "../controllers/payment.controllers.js";
+import { generatePaymentOrder, verifyPaymentController, razorpayWebhookController } from "../controllers/payment.controllers.js";
 
 const paymentRouter = Router();
 
 // Routes for Razorpay payment order
 paymentRouter.route("/create-order").post(generatePaymentOrder);
-paymentRouter.route("/verify-payment").post(verifyPaymentController);
+paymentRouter.route("/verify-payment").post(verifyPaymentController); // payment verification from frontend(user-side)
+// Razorpay webhook for payment confirmation
+paymentRouter.post("/webhook", express.raw({type: "application/json"}), razorpayWebhookController);
 
-export default paymentRouter
+export default paymentRouter;
